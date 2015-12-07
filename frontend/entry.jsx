63a8e8471a89afcd119de415761ebe21d9692820
sheri <sheri.h.zada@gmail.com>
2015-12-07 10:11:39 -0800

    widgets skeleton, configs

---
+++ frontend/entry.jsx
@@ -0,0 +1,6 @@
+var React = require('react'),
+    ReactDOM = require('react-dom');
+
+document.addEventListener('DOMContentLoaded', function(){
+  ReactDOM.render(<widget/>, document.getElementById('root'));
+});
